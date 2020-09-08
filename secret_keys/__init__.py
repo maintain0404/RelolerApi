f11f3341992622dcd0323a1978d618b8ae03743c
Taein Min <maintain0404@gmail.com>
2020-09-08 22:56:10 +0900

    secret_keys added

---
+++ secret_keys/__init__.py
@@ -0,0 +1,16 @@
+import json
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+riot_secret_file = open(os.path.join(current_dir, 'riot_secret_key.json'))
+riot_secret_key = json.load(riot_secret_file)['key']
+
+aws_secret_file = open(os.path.join(current_dir, 'aws_access_key.json'))
+aws_secret = json.load(aws_secret_file)
+
+google_client_secret_path = os.path.join(current_dir, 'google_client_secret.json')
+google_client_secret_file = json.load(open(google_client_secret_path))
+google_client_id = google_client_secret_file['web']['client_id']
+
+django_secret_key = '2k^%70kv(+_688^b^94^v-8f9vwp#nne)40n7*1k^6v!in0zc-'
