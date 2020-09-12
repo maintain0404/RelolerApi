0a5455f1216c189043991341a9319e7d0d141b56
Taein Min <maintain0404@gmail.com>
2020-09-12 17:00:05 +0900

    allowed host modified

--- RelolerApi/setting/develop_eb.py
+++ RelolerApi/setting/develop_eb.py
@@ -31,7 +31,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'elasticbeanstalk.com',
-    'RELOLER-API-dev.ap-northeast-2.elasticbeanstalk.com',
+    'relolerapi-env.eba-r394bymg.ap-northeast-2.elasticbeanstalk.com',
     '127.0.0.1',
     'localhost',
 ]
