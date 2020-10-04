3788d90610febbc1f1ace4dcdf6d324cc95dde20
Taein Min <maintain0404@gmail.com>
2020-10-04 13:31:45 +0900

    Frontpage added to redirect_uri - google drive list api docs added

--- API/views.py
+++ API/views.py
@@ -408,20 +408,20 @@ class GoogleSignInView(APIView):
         return Response(result, status = status.HTTP_200_OK)
 
 class GoogleDriveView(APIView):
+    @swagger_auto_schema(
+        operation_id = 'GoogleDrive_List',
+        operation_description = "구글 드라이브 내의 Reloler 영상 조회",
+        responses = {
+            200 : "조회 성공",
+            400 : "조회 실패"
+        })
     def get(self, request):
-        return Response(
-            drive.Drive(
-                request.session['google_access_token']
-            ).list()
-            ,
-            status = status.HTTP_200_OK
-        )
-
-    def post(self):
-        pass
-
-    def put(self):
-        pass
-
-    def delete(self):
-        pass
+        response = Response()
+        try:
+            video_list = drive.Drive(request.sessions['google_access_token']).list()
+            response.data = video_list
+            response.status = status.HTTP_200_OK
+            return response
+        except Exception:
+            response.status = status.HTTP_404_NOT_FOUND
+            return response
