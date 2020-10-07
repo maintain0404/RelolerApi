9c80f5b09bafe1feba6e855623388e8607c6a7f8
Taein Min <maintain0404@gmail.com>
2020-10-07 15:37:16 +0900

    delete unused things - delete background uploader - delete function get_elb_private_ips

--- RelolerApi/setting/develop_eb.py
+++ RelolerApi/setting/develop_eb.py
@@ -12,36 +12,3 @@ ALLOWED_HOSTS = [
     "172.31.34.104",
 ]
 
-def get_elb_private_ips():
-    import boto3
-    import secret_keys
-    try:
-        elb = boto3.client('elbv2', 
-            region_name = 'ap-northeast-2', 
-            aws_access_key_id = secret_keys.aws_secret['AWS']['Access Key ID'],
-            aws_secret_access_key = secret_keys.aws_secret['AWS']['Security Access Key']
-        )
-        ec2 = boto3.resource(service_name = 'ec2',
-            region_name = 'ap-northeast-2',
-            aws_access_key_id = secret_keys.aws_secret['AWS']['Access Key ID'],
-            aws_secret_access_key = secret_keys.aws_secret['AWS']['Security Access Key']
-        )
-        lbs = []
-        
-        # 모든 로드 밸런서 가져오기
-        for i in elb.describe_load_balancers()['LoadBalancers']:
-            print(i)
-            arn_index = i['LoadBalancerArn'].find('awseb-AWSEB')
-            print(arn_index)
-            if arn_index >= 0:
-                lbs.append(i['LoadBalancerArn'][arn_index:])
-        
-        print(lbs)
-        private_ips = []
-        # 로드 밸런서에 맞는 네트워크 인터페이스 선별
-        for i in ec2.network_interfaces.all():
-            if True in map(lambda x: x in i.description, lbs):
-                private_ips.append(i.private_ip_address)
-        return private_ips
-    except Exception:
-        return []
