6f67591ecd2b1913ea5a34a5e780fd6fbdc964ea
Taein Min <maintain0404@gmail.com>
2020-10-08 16:51:47 +0900

    cors setting applified

--- RelolerApi/setting/common.py
+++ RelolerApi/setting/common.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleWare',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'RelolerApi.urls'
